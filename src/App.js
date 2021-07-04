ad04def7dbc39b0725d55d94748f47562b6f6cf7
LalitSinghRana <lalitrn44@gmail.com>
2021-07-04 17:49:09 +0530

    added buttons for page navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import './App.css';
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	makeStyles,
+	Button,
+	IconButton,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	Button: {
+		margin: theme.spacing(1),
+		color: '#000',
+		backgroundColor: 'orange',
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
 
 function App() {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography>Hello World</Typography>
-      </Toolbar>
-    </AppBar>
-  );
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<AppBar position='static'>
+				<Toolbar>
+					<IconButton
+						edge='start'
+						className={classes.menuButton}
+						color='inherit'
+						aria-label='menu'
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' className={classes.title}>
+						News
+					</Typography>
+					<Button variant='contained' className={classes.Button}>
+						Social Media
+					</Button>
+					<Button variant='contained' className={classes.Button}>
+						My Ranking
+					</Button>
+					<Button variant='contained' className={classes.Button}>
+						Make an entry
+					</Button>
+					<Button variant='contained' className={classes.Button}>
+						Log In/Sign Up
+					</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
 
 export default App;
