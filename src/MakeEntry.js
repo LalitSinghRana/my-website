55901287c1ed0fb8270ca4cdbb900d328d40138d
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2021-07-08 14:22:11 +0530

    created new pages

---
+++ src/MakeEntry.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MakeEntry() {
+	return (
+		<div>
+			<h1>Make an entry to DB</h1>
+		</div>
+	);
+}
---
+++ src/MyRankIt.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MyRankIt() {
+	return (
+		<div>
+			<h1>Drang and Drop</h1>
+		</div>
+	);
+}
